10c25269385b0c768ac44642a2048d1f036d6802
alexh <ahh5238@psu.edu>
2025-10-22 16:22:29 -0400

    Added development comments for each file

---
+++ QiskitShor'sAlgorithmEXAMPLE.py
@@ -0,0 +1,7 @@
+# Project: abcapstonefa25istTeam1
+# Purpose Details: Public example of shor's algorithm hardcoded for the N = 15
+# Course: Fall 2025 Abington Capstone Project Quantum Cryptosystem
+# Author:
+# Date Developed:
+# Last Date Changed:
+# Revision:
