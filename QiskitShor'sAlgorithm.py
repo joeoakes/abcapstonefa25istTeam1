698d941974789ed451a575bbc2260dca3da1a039
alexh <ahh5238@psu.edu>
2025-10-23 18:23:48 -0400

    Updated comment header

--- QiskitShor'sAlgorithm.py
+++ QiskitShor'sAlgorithm.py
@@ -1,7 +1,7 @@
 # Project: abcapstonefa25istTeam1
 # Purpose Details: Shor's algorithm using Qiskit quantum simulation
 # Course: Fall 2025 Abington Capstone Project Quantum Cryptosystem
-# Author:
-# Date Developed:
-# Last Date Changed:
-# Revision:
+# Author: Alex
+# Date Developed: 10/22/2025
+# Last Date Changed: 10/23/2025
+# Revision: Updating comment header
