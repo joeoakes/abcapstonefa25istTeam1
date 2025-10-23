170c5a8c77f5d8b27947362aba8aa0dac67e16fc
aaa7232 <159196968+aaa7232@users.noreply.github.com>
2025-10-23 18:39:19 -0400

    Update SimpleRSAGeneration.py
    Added the code for the function of the RSA generator

--- SimpleRSAGeneration.py
+++ SimpleRSAGeneration.py
@@ -1,7 +1,29 @@
 # Project: abcapstonefa25istTeam1
 # Purpose Details: Simple RSA Encryption Generation
 # Course: Fall 2025 Abington Capstone Project Quantum Cryptosystem
-# Author:
-# Date Developed:
-# Last Date Changed:
-# Revision:
+# Author: Ali Almalky
+# Date Developed: 10/23/2025
+# Last Date Changed: 10/23/2025
+# Revision: 
+
+import math
+from sympy import primerange, isprime
+import random
+
+def generate_rsa_keys(prime_limit=100):
+    primes = list(primerange(2, prime_limit+1))
+    
+    p = random.choice(primes)
+    q = random.choice(primes)
+    while q == p:
+        q = random.choice(primes)
+    
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    
+    possible_es = [x for x in range(2, phi) if math.gcd(x, phi) == 1]
+    e = random.choice(possible_es)
+    
+    d = pow(e, -1, phi)
+    
+    return p, q, n, phi, e, d
